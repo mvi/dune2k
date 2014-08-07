99d543cf83e8c1a05ef6636b76402f243fe63dd1
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2014-08-07 21:56:06 +0200

    fixed a bug where you could not press a mouse key and scroll with the mouse wheel at the same time

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 Dune 2000 1.06p
--
+================================================================================
+Patches/bug fixes for Dune 2000 (by Westwood Studios/Intelligent Games)
+================================================================================
 
-by FunkyFr3sh - http://funkyfr3sh.cncnet.org/
+### Instructions
 
-Required tools:
-http://hifi.iki.fi/win-builds-for-cnc-patch.zip
+ - Download the following file and extract it somewhere: http://hifi.iki.fi/win-builds-for-cnc-patch.zip
+ - Start win-install.bat
+
+Once completed, start build.cmd to compile a fresh dune2000.exe
+
+
+Credits
+--------------------------------------------------------------------------------
+ - FunkyFr3sh
+ - hifi
+ - Sonarpulse
--- src/mousewheel-scrolling.asm
+++ src/mousewheel-scrolling.asm
@@ -1,11 +1,13 @@
 %include "patch.inc"
 
-@LJMP 0x004A3249, WM_MouseHWheel
+@LJMP 0x004A3249, WM_MouseWheel
 @LJMP 0x00444019, ScrollingUp
 @LJMP 0x00443FD3, ScrollingDown
 
-%define UpArrow_Key 0x26
-%define DownArrow_Key 0x28
+%define UpArrowKeyIsDown1 0x26+0x797E78
+%define UpArrowKeyIsDown2 0x26+0x798390
+%define DownArrowKeyIsDown1 0x28+0x797E78
+%define DownArrowKeyIsDown2 0x28+0x798390
 
 [section .idata]
 	MouseWheelTriggered db 0
@@ -13,19 +15,21 @@
 
 [section .text]
 
-WM_MouseHWheel:
+WM_MouseWheel:
 	cmp esi,0x20A
 	jnz .out
 	mov byte [MouseWheelTriggered],1
-	cmp ecx,0x00780000 ;MouseWheel Up
+	ror ecx,0x10
+	cmp cl,0x78 ;MouseWheel Up 0x00780000
+	ror ecx,0x10
 	jnz .MouseWheelDown
-	mov byte [UpArrow_Key+0x797E78],1
-	mov byte [UpArrow_Key+0x798390],1
+	mov byte [UpArrowKeyIsDown1],1
+	mov byte [UpArrowKeyIsDown2],1
 	jmp .out
 	
-.MouseWheelDown ;MouseWheel Down 0xFF880000
-	mov byte [DownArrow_Key+0x797E78],1
-	mov byte [DownArrow_Key+0x798390],1
+.MouseWheelDown: ;MouseWheel Down 0xFF880000
+	mov byte [DownArrowKeyIsDown1],1
+	mov byte [DownArrowKeyIsDown2],1
 	jmp .out
 
 .out:
@@ -38,7 +42,7 @@ ScrollingUp:
 	mov eax,dword [0x516440]
 	cmp byte [MouseWheelTriggered],1
 	jnz 0x0044401E
-	mov byte [UpArrow_Key+0x797E78],0
+	mov byte [UpArrowKeyIsDown1],0
 	mov byte [MouseWheelTriggered],0
 	jmp 0x0044401E
 
@@ -47,6 +51,6 @@ ScrollingDown:
 	mov eax,dword [0x516440]
 	cmp byte [MouseWheelTriggered],1
 	jnz 0x00443FD8
-	mov byte [DownArrow_Key+0x797E78],0
+	mov byte [DownArrowKeyIsDown1],0
 	mov byte [MouseWheelTriggered],0
 	jmp 0x00443FD8
