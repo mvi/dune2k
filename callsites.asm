b599fd376ed60fd97fac055c7b6f071b7809690e
John Ericson <Ericson2314@Yahoo.com>
2014-08-08 17:04:54 -0400

    Add documentation to callsites.asm and sym.asm

--- callsites.asm
+++ callsites.asm
@@ -1,10 +1,18 @@
-;
-; This file is strictly for adding relocations to calls/jmps/etc that
-; exist in the vanilla exe. Write your real patches elsewhere.
-;
+;;
+;; This file is strictly for adding relocations to calls/jmps/etc that
+;; exist in the vanilla exe.
+;;
+;; The idea is this allows us to cleanly do whole function
+;; replacement, and all callsites in the original binary are updated
+;; automatically.
+;;
+;; It is important not to add any patch jumps/calls here. That way we
+;; can just link this, and diff this and the vanilla binary to make
+;; sure all of these are correct.
+;;
 
 %include "macros/patch.inc"
 
 ;%include "main.inc"
 
-;@CALL 0x007CD8EA, WinMain@16 ; _start calls _WinMain@16. Should be only callsite of WinMain@16
+;@CALL <mystery-addr>, WinMain@16 ; _start calls _WinMain@16. Should be only callsite of WinMain@16
--- sym.asm
+++ sym.asm
@@ -1,4 +1,16 @@
+;;
+;; This file exists to define the locations of everything we know about the
+;; vanilla exe.
+;;
+;; The idea is by separating knowledge about the vanilla exe from changes to it,
+;; we make the code more clean. Also it means we don't need to hard-code the
+;; symbols separately for C, C++, asm, etc.
+;;
+;; Symbols are defined with `setcglob` for C (remove leading underscore)
+;;                     and  `setxglob` for C++ and C
+;;
+
 %include "macros/setsym.inc"
 
 setcglob 0x004B2810, start
-;setcglob 0x006BB9A0, WinMain@16
+;setcglob <mystery-addr>, WinMain@16
