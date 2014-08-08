418a6fbc83cd9038a8bf52c8ce1c706b79205120
John Ericson <Ericson2314@Yahoo.com>
2014-08-08 16:45:37 -0400

    Use fancy macros in mouse-wheel-and-buttons.asm

--- src/mouse-wheel-and-buttons.asm
+++ src/mouse-wheel-and-buttons.asm
@@ -1,46 +1,45 @@
 %include "macros/patch.inc"
-
-@LJMP 0x004A3249, WindowMessage
-@LJMP 0x00444019, WheelScrollingUp
-@LJMP 0x00443FD3, WheelScrollingDown
+%include "macros/hack.inc"
 
 extern _HandleWindowMessage
 extern _MouseWheelTriggered
 
-%define UpArrowKeyIsDown1 0x26+0x797E78
-%define DownArrowKeyIsDown1 0x28+0x797E78
+%assign UpArrowKeyIsDown1 0x26+0x797E78
+%assign DownArrowKeyIsDown1 0x28+0x797E78
 
-WindowMessage:
-	push ecx 
-	push edx
-	push eax
-	push esi
-	push ecx
-	push esi
-	call _HandleWindowMessage
-	add esp,8
-	pop esi
-	pop eax
-	pop edx
-	pop ecx
-	cmp esi,0x14
-	ja 0x004A328F
-	jmp 0x004A324E
-	
-
-WheelScrollingUp:
-	mov eax,dword [0x516440]
-	cmp byte [_MouseWheelTriggered],1
-	jnz 0x0044401E
-	mov byte [UpArrowKeyIsDown1],0
-	mov byte [_MouseWheelTriggered],0
-	jmp 0x0044401E
+@HACK 0x004A3249, WindowMessage
+    push ecx
+    push edx
+    push eax
+    push esi
+    push ecx
+    push esi
+    call _HandleWindowMessage
+    add  esp,8
+    pop  esi
+    pop  eax
+    pop  edx
+    pop  ecx
+    cmp  esi,0x14
+    ja   0x004A328F
+    jmp  0x004A324E
+@ENDHACK
 
 
-WheelScrollingDown:
-	mov eax,dword [0x516440]
-	cmp byte [_MouseWheelTriggered],1
-	jnz 0x00443FD8
-	mov byte [DownArrowKeyIsDown1],0
-	mov byte [_MouseWheelTriggered],0
-	jmp 0x00443FD8
+@HACK 0x00444019, WheelScrollingUp
+    mov  eax,dword [0x516440]
+    cmp  byte [_MouseWheelTriggered],1
+    jnz  0x0044401E
+    mov  byte [UpArrowKeyIsDown1],0
+    mov  byte [_MouseWheelTriggered],0
+    jmp  0x0044401E
+@ENDHACK
+
+@HACK 0x00443FD3, WheelScrollingDown
+    mov  eax,dword [0x516440]
+    cmp  byte [_MouseWheelTriggered],1
+    jnz  0x00443FD8
+    mov  byte [DownArrowKeyIsDown1],0
+    mov  byte [_MouseWheelTriggered],0
+    jmp  0x00443FD8
+@ENDHACK
