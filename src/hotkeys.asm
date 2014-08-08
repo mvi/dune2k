959da080e5d47b2c0e7cee8df5efcf8d09cb3b15
John Ericson <Ericson2314@Yahoo.com>
2014-08-08 16:23:31 -0400

    Add @REPLACE macro, use in hotkeys.asm

--- src/hotkeys.asm
+++ src/hotkeys.asm
@@ -1,42 +1,39 @@
 %include "macros/patch.inc"
-
-@CLEAR 0x004A33A4, 0x90, 0x004A33B0
-@LJMP 0x004A33A4, WM_KeyDown
-
-@CLEAR 0x004A3468, 0x90, 0x004A346F
-@LJMP 0x004A3468, WM_KeyUp
+%include "macros/hack.inc"
 
 ; this tells nasm that the C symbol (function) "HandleKeyEvent" is external of this file - linkers problem not ours to resolve it
 extern _HandleKeyEvent
 
 ; two cdecl calls with two real arguments and should leave the original registers intact because we pop them back in
-WM_KeyDown:
-	push ecx 
-	push edx
-	push eax
-	push esi
-	push 1
-	push esi
-	call _HandleKeyEvent
-	add esp,8
-	pop esi
-	pop eax
-	pop edx
-	pop ecx
-	jmp 0x4A33B0
 
-	
-WM_KeyUp:
-	push esi
-	push ecx 
-	push edx
-	push eax
-	push 0
-	push eax
-	call _HandleKeyEvent
-	add esp,8
-	pop eax
-	pop edx
-	pop ecx
-	pop esi
-	jmp 0x4A346F
+@REPLACE 0x004A33A4, 0x4A33B0, WM_KeyDown
+    push ecx
+    push edx
+    push eax
+    push esi
+    push 1
+    push esi
+    call _HandleKeyEvent
+    add esp,8
+    pop esi
+    pop eax
+    pop edx
+    pop ecx
+    jmp %$end
+@ENDREPLACE
+
+@REPLACE 0x004A3468, 0x4A346F, WM_KeyUp
+    push esi
+    push ecx
+    push edx
+    push eax
+    push 0
+    push eax
+    call _HandleKeyEvent
+    add esp,8
+    pop eax
+    pop edx
+    pop ecx
+    pop esi
+    jmp %$end
+@ENDREPLACE
