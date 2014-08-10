13f1857ca47afc268bf2456d00f71b98e4d2de01
Barnaby Smith <barnabysmith@gmail.com>
2014-08-10 12:21:33 +0100

    Added a shared header for Dune vars/functions
    Will split this header up later

---
+++ inc/vanillashared.h
@@ -0,0 +1,9 @@
+// This header works with asym.sym which defines the Vanilla symbols
+// This header will be split up as it becomes larger
+
+// Variables
+extern int MousePositionX;
+extern int MousePositionY;
+
+// Functions
+unsigned __int8 __fastcall _Text_On_Screen(int a1, int a2, int a3, int a4, int a5, int a6, unsigned __int8 a7, int a8, int a9);
