67029420760d20582708303987eb52fa030bd305
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2014-08-11 04:47:51 +0200

    more global vars

--- inc/vanillashared.h
+++ inc/vanillashared.h
@@ -1,28 +1,86 @@
 // This header works with sym.asm which defines the Vanilla symbols
 // This header will be split up as it becomes larger
 
-// Global Variables
+// ### Constants ###
+
+enum Sides {
+	Atreides = 0,
+	Harkonnen,
+	Ordos
+};
+
+enum Colors {
+	Blue = 0,
+	Red,
+	Teal,
+	Purple,
+	Gray,
+	Brown,
+	Gold,
+	LightBrown
+};
+
+// ### Global Variables ###
+
+//Network settings
+extern unsigned int SendRate;
+extern unsigned int ReconnectTimeOutToPlayer;
+extern unsigned int InitialConnectTimeOut;
+
+extern unsigned int UnknownNetVar1;
+extern unsigned int UnknownNetVar2;
+extern unsigned int UnknownNetVar3;
+extern unsigned int UnknownNetVar4;
+extern unsigned int UnknownNetVar5;
+
+//Others
 extern int MousePositionX;
 extern int MousePositionY;
 
-extern int MySideID;
+extern unsigned char MySideID;
 extern bool Win;
 extern bool Lose;
-extern int DifficultyLevel;
+extern unsigned char DifficultyLevel;
 extern int GameType;
 
 extern char NetworkGame;
-extern int NetUnitCount;
-extern int NetTechLevel;
-extern int NetAIPlayers;
-extern int MaxPlayers;
+
+//Multiplayer settings
+extern bool FirstTimePlay;
+extern unsigned char NetUnitCount;
+extern unsigned char NetTechLevel;
+extern unsigned short NetStartingCredits;
+extern unsigned char NetAIPlayers;
+extern bool NetCrates;
+extern unsigned char NetWorms;
+extern unsigned char NetPlayerName[];
+extern unsigned char NetPlayerSide;
+extern unsigned char NetPlayerColour;
+extern unsigned char NetIPAddress[];
+extern unsigned char NetModemPhone[];
+extern unsigned char NetSerialComPort;
+
+//Game settings
+extern unsigned int ScrollRate;
+extern unsigned short GameSpeed;
+extern unsigned int GameBitsPerPixel;
+extern unsigned int SFXVolume;
+extern unsigned int MusicVolume;
+
+//WOL
+extern bool AllowFindUser;
+extern bool AllowPageUser;
+extern bool LanguageFilter;
+extern bool ShowAllGames;
+extern bool Microphone;
+
+//Others
+extern unsigned char MaxPlayers;
 extern bool TournamentGame;
 extern bool PrivateGame;
-extern bool NetCrates;
-extern int NetWorms;
-extern int NetPlayerHandicap;
+extern unsigned char NetPlayerHandicap;
 
-extern int AtreidesMoney;
+extern unsigned int AtreidesMoney;
 
 extern bool DebugMode;
 
--- sym.asm
+++ sym.asm
@@ -28,7 +28,6 @@ setcglob 0x4E3AF0, UnknownNetVar4
 setcglob 0x4E3AF4, UnknownNetVar5
 
 ;Others
-
 setcglob 0x004EB048, MousePositionX
 setcglob 0x004EB04C, MousePositionY
 
@@ -39,14 +38,40 @@ setcglob 0x004E8BF0, DifficultyLevel
 setcglob 0x00797E34, GameType
 
 setcglob 0x007984C4, NetworkGame
+
+;Multiplayer settings
+setcglob 0x004E7ACC, FirstTimePlay
 setcglob 0x004E3B00, NetUnitCount
 setcglob 0x004E3B04, NetTechLevel
+setcglob 0x004E3B08, NetStartingCredits
 setcglob 0x004E3B0C, NetAIPlayers
+setcglob 0x006B9834, NetCrates
+setcglob 0x006B9838, NetWorms
+setcglob 0x006B93F8, NetPlayerName
+setcglob 0x006B9840, NetPlayerSide
+setcglob 0x006B9844, NetPlayerColour
+setcglob 0x007975D0, NetIPAddress
+setcglob 0x00797960, NetModemPhone
+setcglob 0x004E8BF4, NetSerialComPort
+
+;Game settings
+setcglob 0x004EB030, ScrollRate
+setcglob 0x004E3B18, GameSpeed
+setcglob 0x004EB01C, GameBitsPerPixel
+setcglob 0x004E5FC4, SFXVolume
+setcglob 0x004E5FC8, MusicVolume
+
+;WOL
+setcglob 0x004DB9D4, AllowFindUser
+setcglob 0x004DB9D8, AllowPageUser
+setcglob 0x004F3D00, LanguageFilter
+setcglob 0x004F3D04, ShowAllGames
+setcglob 0x004F3CF0, Microphone
+
+;others
 setcglob 0x004E8BEC, MaxPlayers
 setcglob 0x004F3D3C, TournamentGame
 setcglob 0x004F3D40, PrivateGame
-setcglob 0x006B9834, NetCrates
-setcglob 0x006B9838, NetWorms
 setcglob 0x006B9848, NetPlayerHandicap
 
 setcglob 0x007BCACC, AtreidesMoney
